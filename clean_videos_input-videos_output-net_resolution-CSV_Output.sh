9dd75306d3917377219b20976366e5d2983a7753
ai4hero <59150746+ai4hero@users.noreply.github.com>
2020-05-07 10:26:20 +0800

    Add files via upload

---
+++ clean_videos_input-videos_output-net_resolution-CSV_Output.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# clean
+rm -rf ~/project/openpose/videos_input/*
+rm -rf ~/project/openpose/net_resolution/*
+rm -rf ~/project/openpose/videos_output/*
+rm -rf ~/tools/data2csv/CSV_Output/*
+
+
+
+
